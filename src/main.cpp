20b7924e67139c3da58213a5290a9d2ba255984b
Lars Kaiser <lars.kaiser@mathematik.uni-stuttgart.de>
2021-02-09 19:27:34 +0100

    [Template] Add reusable files and folders

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  // if the number of given command line arguments is only 1 (= the program name), print out usage information and exit
+  if (argc == 1)
+  {
+    std::cout << "usage: " << argv[0] << " <filename>" << std::endl;
+
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
